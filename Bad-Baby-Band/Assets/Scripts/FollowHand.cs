6589b01a524cb55ee3403a73ee74561e2a3b3ca4
John Manning <john.manning144@gmail.com>
2021-10-03 18:19:10 -0400

    Game Works

--- Bad-Baby-Band/Assets/Scripts/FollowHand.cs
+++ Bad-Baby-Band/Assets/Scripts/FollowHand.cs
@@ -7,10 +7,7 @@ public class FollowHand : MonoBehaviour
     private Rigidbody rb;
     private GameObject parent;
     private bool held;
-    private bool dragging;
 
-    private Vector3 f;
-    private Vector3 roi = new Vector3(0f, 0.1f, 0f);
     private Vector3 dragOrigin;
     private Vector3 dragRelease;
     // Start is called before the first frame update
@@ -19,36 +16,39 @@ public class FollowHand : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody>();
         parent = GameObject.Find("PlayerHand");
         held = true;
-        dragging = false;
-        f = new Vector3(0,0,0);
+        rb.useGravity = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(transform.position);
+        //Debug.Log(transform.position);
 
         if(held){
             transform.position = new Vector3(
                 parent.transform.position.x, parent.transform.position.y, parent.transform.position.z);
             if(Input.GetMouseButtonDown(0)){
                dragOrigin = parent.transform.position;
-               dragging = true;
+
             }else if(Input.GetMouseButtonUp(0)){
                 dragRelease = parent.transform.position;
-                dragging = false;
                 held = false;
-                rb.AddForce(0f, 50f, 50f);
                 Throw();
             }
         }
-        // if(Input.GetMouseButton(0) && dragging){
-        // }
     }
     void Throw(){   
-     //  velocity = new Vector3(dragRelease.x - dragOrigin.x,dragRelease.y - dragOrigin.y, dragRelease.z- dragOrigin.z );
-        rb.AddForce(0f, 10f, 10f, ForceMode.Impulse);
+        float thrust = 0.05f;
+        Vector3 throwVector = dragRelease - dragOrigin;
+        throwVector.z = throwVector.y;
 
+        float speed = throwVector.magnitude / Time.deltaTime;
+        //Debug.Log("speed: " + speed);
+        Vector3 throwVelocity = speed * throwVector.normalized;
+        
+
+        rb.useGravity = true;
+        rb.velocity = throwVelocity;
         FMODUnity.RuntimeManager.PlayOneShot("event:/SFX02 - Yeet");
         Debug.Log("Thrown");
     }
